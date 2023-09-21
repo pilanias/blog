c163e0673804f6ec4a3d64a96444b5c121e9468a
pilanias38 <pilanias38@gmail.com>
2023-09-21 10:19:33 +0530

    clarity code

--- components/SEO.js
+++ components/SEO.js
@@ -39,6 +39,16 @@ const CommonSEO = ({ title, description, ogType, ogImage, twImage, canonicalUrl 
           gtag('config', 'G-4QQ0HZWMM3');
         `}
       </script>
+      {/* Include Clarity script here */}
+      <script type="text/javascript">
+        {`
+          (function(c,l,a,r,i,t,y){
+            c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "iygdui474y");
+        `}
+      </script>
     </Head>
   )
 }
